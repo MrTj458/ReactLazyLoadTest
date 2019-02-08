86e92b3a25ae6e93cbd222d9a8771b82cff62978
Trevor Hodsdon <trevorhodsdon@Trevors-MacBook-Pro.local>
2019-02-07 23:12:51 -0700

    added imports

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import React from 'react'
+import React, { Suspense, lazy } from 'react'
 import './App.css'
 import { Switch, Route } from 'react-router-dom'
 
-const Nav = React.lazy(() => import('./components/Nav'))
-const Home = React.lazy(() => import('./components/Home'))
-const About = React.lazy(() => import('./components/About'))
-const NotFound = React.lazy(() => import('./components/NotFound'))
+const Nav = lazy(() => import('./components/Nav'))
+const Home = lazy(() => import('./components/Home'))
+const About = lazy(() => import('./components/About'))
+const NotFound = lazy(() => import('./components/NotFound'))
 
 function App() {
 	return (
 		<>
-			<React.Suspense
+			<Suspense
 				fallback={
 					<h1 style={{ textAlign: 'center', marginTop: '10%' }}>Loading...</h1>
 				}
@@ -21,7 +21,7 @@ function App() {
 					<Route exact path="/about" component={About} />
 					<Route component={NotFound} />
 				</Switch>
-			</React.Suspense>
+			</Suspense>
 		</>
 	)
 }
